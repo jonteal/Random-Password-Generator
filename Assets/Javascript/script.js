eaeb5033ccb2d5045f225e9a6854b9d5d57b59a3
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-11 17:09:48 -0500

    Created if and for loops, and assigned variables at top

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,56 +1,92 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var storedPassword = [];
+var numberLength = 0;
+var finalPassword = "";
+
 
 // Write password to the #password input
-function writePassword(upper, lower, number, character, length) {
+function writePassword() {
+
+  var numberRange = prompt("Please enter a number from 8-128")
+
+if((numberRange < 8) || (numberRange > 128)) {
+  alert('Please pick a number between 8-128!')
+}
+else{
+  numberLength = numberRange;
+}
+  
+  var uppercase = confirm('Do you want to use uppercase letters?');
+  var lowercase = confirm('Do you want to use lowercase letters?');
+  var numbers = confirm('Do you want to use numbers?');
+  var character = confirm('Do you want to use special characters?');
+
+  // Random Character Generate Functions 
+
+// Random uppercase letter generator
+if(uppercase) {
+  for (let i = 65; i <= 90; i++) {
+    storedPassword.push(i);
+  }
+alert(storedPassword);
+
+// Random lowercase letter generator
+if(uppercase) {
+  for (let i = 97; i <= 122; i++) {
+    storedPassword.push(i);
+  }
+}
+alert(storedPassword);
+
+// Random number generator
+if(number) {
+  for (let i = 0; i <= 9; i++) {
+    storedPassword.push(i);
+  }
+}
+alert(storedPassword);
+
+
+// Random special character generator
+if(character) {
+  for (let i = 34; i <= 126; i++){
+    if(i <= 48 && i >= 57) {
+      continue;
+      }
+    if(i >= 65 && i >= 122){
+      continue;
+    }
+    else {
+      storedPassword.push(i);
+    }
+    }
+}
+alert(storedPassword);
+
+
+
+for(i = 0; i < numberLength; i++) {
+  var randomChar = Math.floor(Math.random() * storedPassword.length);
+  alert(randomChar);
+  finalPassword += String.fromCharCode(storedPassword[randomChar]);
+}
+
+
+alert(finalPassword);
+  
+  
+  
   var password = generatePassword();
-
-
-
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+}
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", ());
-  
+generateBtn.addEventListener("click", writePassword);
 
 
-
-
-// Random Character Generator Object
-const randomCharFunction = {
-  upper: pickRandomUpper,
-  lower: pickRandomLower,
-  number: pickRandomNumber,
-  character: pickRandomCharacter
-};
-
-
-// Random Character Generate Functions 
-
-// Random uppercase letter generator
-function pickRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-// Random lowercase letter generator
-function pickRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-// Random number generator
-function pickRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-// Random special character generator
-function pickRandomCharacter() {
-  const character = '<>{})(*&^%$#@!';
-  return character[Math.floor(Math.random() * character.length)];
-}
-
-console.log(pickRandomCharacter());
