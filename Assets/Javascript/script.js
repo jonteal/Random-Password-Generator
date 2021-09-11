f3b0540709e15e8f8d11767e1d7889ad23bc7c90
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-11 10:36:49 -0500

    Random functions created

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -2,8 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(upper, lower, number, character, length) {
   var password = generatePassword();
+
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,4 +15,42 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", ());
+  
+
+
+
+
+// Random Character Generator Object
+const randomCharFunction = {
+  upper: pickRandomUpper,
+  lower: pickRandomLower,
+  number: pickRandomNumber,
+  character: pickRandomCharacter
+};
+
+
+// Random Character Generate Functions 
+
+// Random uppercase letter generator
+function pickRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// Random lowercase letter generator
+function pickRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// Random number generator
+function pickRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// Random special character generator
+function pickRandomCharacter() {
+  const character = '<>{})(*&^%$#@!';
+  return character[Math.floor(Math.random() * character.length)];
+}
+
+console.log(pickRandomCharacter());
