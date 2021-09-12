2df45998cc699e3c60904137fd842b7d69f9d0cc
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-12 12:22:10 -0500

    Added variable and if statement for numberRange

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -4,3 +4,13 @@ var storedPassword = [];
 var numberLength = 0;
 var finalPassword = "";
 
+// Write password to the password input
+function writePassword() {
+  var numberRange = prompt('Please enter a number between 8 - 128.')
+
+  if((numberRange < 8) || (numberRange > 128)) {
+    alert('Please pick a number between 8 - 128!')
+  }
+  numberLength = numberRange;
+}
+
