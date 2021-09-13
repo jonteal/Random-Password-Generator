2718a4da500e49c37b9392061c79f6ba9d83ca23
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-12 22:19:43 -0500

    Setting up Function that will eventually pass finalPassword to text area

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,15 +1,27 @@
 // Assigning Variables
 
 var storedPassword;
-var randomUppercase;
-var randomLowercase;
-var randomNumber;
-var randomSymbol;
+var useUppercase;
+var useLowercase;
+var useNumbers;
+var useSymbols;
 
 
 randomUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 randomLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-randomNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-randomSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "|", "/", "}", "{", "[", "]", "<", ">", "?", "-", "+"];
+randomNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+randomSymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "|", "/", "}", "{", "[", "]", "<", ">", "?", "-", "+"];
 
-var 
+var selectedCriteria;
+
+var generate = document.querySelector("#generate");
+
+generate.addEventListener("click", function () {
+    finalPassword = generatePassword();
+    document.getElementById("password").placeholder = finalPassword;
+}
+
+);
+
+var passwordLength = []
+var finalPassword = passwordLength.join("");
