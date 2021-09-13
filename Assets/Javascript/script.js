c68bdf65f88014ede34e604a241fb9f3a0d79887
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-12 23:35:56 -0500

    Added Lots of Comments

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,36 +1,45 @@
 // Assigning Variables
-
 var userInput;
 var useUppercase;
 var useLowercase;
 var useNumbers;
 var useSymbols;
 
-
+// Listing out Full Arrays of Possible Characters for 
+// Uppercase, Lowercase, Numbers, and  Symbols
 randomUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 randomLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 randomNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 randomSymbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "|", "/", "}", "{", "[", "]", "<", ">", "?", "-", "+"];
 
+// Declaring Variable to Use Validating Selected Criteria Below
 var selectedCriteria;
 
+// Variable to Select the "Generate" Button in HTML
 var generate = document.querySelector("#generate");
 
+// Click Event to Begin Prompts
 generate.addEventListener("click", function () {
     finalPassword = generatePassword();
     document.getElementById("password").placeholder = finalPassword;
 });
 
 
+// Generate Password Function
 function generatePassword() {
 
+// Choose a Number between 8 - 128
     userInput = parseInt(prompt("Choose a number between 8 - 128!"));
 
+// If Statement for if No Selection is Made
     if (!userInput) {
         alert("In order to proceed, you must enter a number.");
+
+// If Statement for if Selection is outside of Specified Range
     } else if (userInput < 8 || userInput > 128) {
         userInput = parseInt(prompt("The number you enter must be between 8 - 128."));
 
+// Else Confirm Promps to Select for Uppercase, Lowercase, Numbers, or Symbols
     } else {
         useUppercase = confirm("Do you wish to include uppercase letters?");
         useLowercase = confirm("Do you wish to include lowercase letters?");
@@ -113,11 +122,12 @@ for (var i = 0; i < userInput; i++) {
     passwordLength.push(randomPassword);
 }
 
-
+// Variable to Hold Password Once Random Outputs have been Joined
 var finalPassword = passwordLength.join("");
 Input(finalPassword);
 return finalPassword;
 
+// Input Final Password into Text Area on Page
 function Input(finalPassword) {
     document.getElementById("password").textContent = finalPassword;
 }
